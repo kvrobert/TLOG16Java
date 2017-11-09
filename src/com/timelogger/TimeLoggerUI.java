4bd60581d233cad931459179d0eb9ce5e18c7327
rkissvincze <rkissvincze@precognox.com>
2017-11-09 14:40:24 +0100

    The functions 0 - 4 were created..

--- src/com/timelogger/TimeLoggerUI.java
+++ src/com/timelogger/TimeLoggerUI.java
@@ -12,10 +12,11 @@ import java.util.stream.IntStream;
 public class TimeLoggerUI {
     
     
-    private Scanner scanner = new Scanner(System.in);
+    private Scanner scanner = new Scanner(System.in);    
     private TimeLogger timeLogger;
     private WorkDay workDay;
     private WorkMonth workMonth;
+    private Task task;
     private final List<String> MENU = Arrays.asList("Exit",
                                                     "List the months",
                                                     "List the days",
@@ -33,55 +34,101 @@ public class TimeLoggerUI {
     }
     
     public void printMenu(){
-        System.out.println("\n::: MENU :::");
-        System.out.println  ("============\n");
-        IntStream.range(0, MENU.size()).forEach(index -> System.out.println(index + ". " + MENU.get(index)));
-        System.out.println("\nChoose a menu, pleas!");
-    }
-    
-    public void selectMenu(){
-    
-        while(true){
         
-            switch(scanner.nextInt())
-            {
-                case 0: exit(); break;
-                case 1: listMonths(); break;
-                case 2: listDays(); break;
-                case 3: listTasks(); break;
-                case 4: addNewMonth(); break;
-                case 5: addNewDay(); break;
-                case 6: createNewTask(); break;
-                case 7: finishATask(); break;
-                case 8: deleteATask(); break;
-                case 9: modifyATask(); break;
-                case 10: statistics(); break;
-                default: System.out.println("Wrong menu! Please choose a correct menu!"); break;
-            }
+        while(true){
+            System.out.println("\n::: MENU :::");
+            System.out.println  ("============\n");
+            IntStream.range(0, MENU.size()).forEach(index -> System.out.println(index + ". " + MENU.get(index)));
+            System.out.println("\nChoose a menu, pleas!");
+            selectMenu();
         }
+    }
+    public void selectMenu(){   
+        
+        switch(scanner.nextInt())
+        {
+            case 0: exit(); break;
+            case 1: listMonths(); break;
+            case 2: listDays(); break;
+            case 3: listTasks(); break;
+            case 4: addNewMonth(); break;
+            case 5: addNewDay(); break;
+            case 6: createNewTask(); break;
+            case 7: finishATask(); break;
+            case 8: deleteATask(); break;
+            case 9: modifyATask(); break;
+            case 10: statistics(); break;
+            default: System.out.println("Wrong menu! Please choose a correct menu!"); break;
+        }
+        
     }
 
     private void exit() {
+        
         System.exit(0);
     }
 
     private void listMonths() {
+        
         System.out.println("ListMonts....");
         if( timeLogger.getMonths().size() > 0 ){
-            IntStream.range(0, timeLogger.getMonths().size()).forEach(index -> System.out.println(index+1 + ". " + timeLogger.getMonths().get(index).toString()));
+            
+            IntStream.range(0, timeLogger.getMonths().size())
+                    .forEach(index -> System.out.println(index+1 + ". "
+                            + timeLogger.getMonths().get(index).toString()));
+            return;
         }
     }
 
     private void listDays() {
-        System.out.println("listDays");
+        
+        listMonths();
+        selectMonth();
+        if( workMonth.getDays().size() > 0){
+        
+            IntStream.range(0, workMonth.getDays().size())
+                    .forEach(index -> System.out.println( index+1 + ". " 
+                            + workMonth.getDays().get(index) ));
+            return;
+        }
+    }
+
+    private void selectMonth() {
+        
+        System.out.println("Chosse a month!");
+        workMonth = timeLogger.getMonths().get(scanner.nextInt() -1);
     }
     
+    private void selectDay() {
+        
+        System.out.println("Chosse a day!");
+        workDay = workMonth.getDays().get(scanner.nextInt() - 1);
+    }
+    
+    private void selectTask() {
+        
+        System.out.println("Chosse a task!");
+        task = workDay.getTasks().get(scanner.nextInt());
+    }
+      
     private void listTasks() {
-        System.out.println("listTasks");
+        listDays();
+        selectDay();
+        if( workDay.getTasks().size() > 0 ){
+            IntStream.range(0, workDay.getTasks().size())
+                    .forEach(index -> System.out.println( index+1 + ". " 
+                            + workDay.getTasks().get(index) )); 
+            return;       
+        }
     }
-    
+
     private void addNewMonth() {
-        System.out.println("addNewMonth");
+        
+        System.out.println("Type the date or press enter to use this month (YYYYMM).");
+        String input = scanner.nextLine();
+        if( input == "" ) timeLogger.addMonth(new WorkMonth());
+        if( input.matches("[1-2][0|9]\\d{2}[0-1][0-2]"  ) )timeLogger.addMonth(new WorkMonth(input));
+        
     }
 
     private void addNewDay() {
