86f1f3779cc7e904f35068efc8ac644d4f9975f8
rkissvincze <rkissvincze@precognox.com>
2017-11-10 16:34:39 +0100

    Task class test finished

--- test/com/timelogger/WorkDayTest.java
+++ test/com/timelogger/WorkDayTest.java
@@ -16,86 +16,30 @@ import org.junit.Before;
  */
 public class WorkDayTest {
     
-    public WorkDayTest() {
-    }
-
-    @AfterClass
-    public static void tearDownClass() throws Exception {
-    }
-
-    @Before
-    public void setUp() throws Exception {
-     //   WorkDay wd1 = new WorkDay
-    }
-
-    /**
-     * Test of getRequiredMinPerDay method, of class WorkDay.
-     */
-    @Test
-    public void testGetRequiredMinPerDay() {
-    }
+ 
 
     /**
      * Test of getSumPerDay method, of class WorkDay.
      */
     @Test
-    public void testGetSumPerDay() {
+    public void testGetExtraMinutesPerDay() throws NotExpectedTimeOrderException, EmptyTimeFieldException, InvalidTaskIdException, InvalidTaskIdException, NoTaskIdException {
+        
+        Task t1 = new Task("4545", "Valami task" , "07:30", "08:45");
+        WorkDay wd1 = new WorkDay();
+        wd1.addTask(t1);
+        long exRes = -375;
+        long result = wd1.getExtraMinPerDay();
+        assertEquals(exRes, result);
     }
-
-    /**
-     * Test of getActualDay method, of class WorkDay.
-     */
+    
     @Test
-    public void testGetActualDay() {
-    }
-
-    /**
-     * Test of getTasks method, of class WorkDay.
-     */
-    @Test
-    public void testGetTasks() {
-    }
-
-    /**
-     * Test of getExtraMinPerDay method, of class WorkDay.
-     */
-    @Test
-    public void testGetExtraMinPerDay() {
-    }
-
-    /**
-     * Test of getLastTaskEndTime method, of class WorkDay.
-     */
-    @Test
-    public void testGetLastTaskEndTime() {
-    }
-
-    /**
-     * Test of setRequiredMinPerDay method, of class WorkDay.
-     */
-    @Test
-    public void testSetRequiredMinPerDay() {
-    }
-
-    /**
-     * Test of setActualDay method, of class WorkDay.
-     */
-    @Test
-    public void testSetActualDay() {
-    }
-
-    /**
-     * Test of addTask method, of class WorkDay.
-     */
-    @Test
-    public void testAddTask() {
-    }
-
-    /**
-     * Test of toString method, of class WorkDay.
-     */
-    @Test
-    public void testToString() {
+    public void testGetExtraMinutesPerDay2() throws NotExpectedTimeOrderException, EmptyTimeFieldException, InvalidTaskIdException, InvalidTaskIdException, NoTaskIdException {
+        
+        Task t1 = new Task("4545", "Valami task" , "07:30", "08:45");
+        WorkDay wd1 = new WorkDay();        
+        long result = wd1.getExtraMinPerDay();
+        assertTrue(result < 0);
+        assertTrue(result == -450);
     }
     
 }
