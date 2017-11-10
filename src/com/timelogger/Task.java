0ba79a4f392f87cd8f60be76f5ddd132cc3061a6
rkissvincze <rkissvincze@precognox.com>
2017-11-10 14:10:33 +0100

    Constructors re editing in Task

--- src/com/timelogger/Task.java
+++ src/com/timelogger/Task.java
@@ -21,29 +21,36 @@ public class Task {
     private LocalTime endTime;
     private String comment;
     
-    public Task(String taskId, String comment, int startHour, int startMin, int endHour, int endMin){
-        
-        this.taskID = taskId;
-        this.comment = comment;
-        this.startTime = LocalTime.of(startHour, startMin);
-        this.endTime = LocalTime.of(endHour, endMin);        
-    }
-    
-    public Task(String taskId, String commnet, String startTime, String endTime){
-        
-        this.taskID = taskId;
-        this.comment = commnet;
-        this.startTime = LocalTime.parse(startTime, DateTimeFormatter.ISO_TIME);
-        if ( endTime != null )this.endTime = LocalTime.parse(endTime, DateTimeFormatter.ISO_TIME);
-        
-    }
-    
+   
     public Task(String taskId, String comment, LocalTime startTime, LocalTime endtime){
         
         this.taskID = taskId;
         this.comment = comment;
         this.startTime = startTime;
         this.endTime = endtime;        
+    }
+    
+    public Task(String taskId, String comment, int startHour, int startMin, int endHour, int endMin){        
+    
+        this( taskId, comment, LocalTime.of(startHour, startMin), LocalTime.of(endHour, endMin));
+    }
+    
+    public Task(String taskId, String commnet, String startTime, String endTime){
+        
+        this( taskId, commnet, LocalTime.parse(startTime, DateTimeFormatter.ISO_TIME), LocalTime.parse(endTime, DateTimeFormatter.ISO_TIME)  );
+        
+    }
+    
+    public Task(String taskId, String commnet, String startTime){
+        
+        this( taskId, commnet, LocalTime.parse(startTime, DateTimeFormatter.ISO_TIME), null  );
+        
+    }
+    
+    public Task(String taskId, String commnet, LocalTime startTime){
+        
+        this( taskId, commnet, startTime, null  );
+        
     }
     
     public Task(String taskId){
